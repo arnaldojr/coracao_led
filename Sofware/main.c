f0fd286105c6073cfe1565e1af32161a398a1307
arnaldojr <arnaldo_viana@yahoo.com.br>
2014-07-26 22:18:50 -0300

    acende um por vez
    acende todos leds no sentido anti-horario, depois no sentido horario.

--- Sofware/main.c
+++ Sofware/main.c
@@ -17,22 +17,23 @@ void main()
 	TRISD = 0x00;		// RD0 ATÉ RD7 COMO SAIDA PARA OS LEDS 
 	TRISE = 0x00;		// RE0 ATÉ RE2 COMO SAIDA
 	
-	/*
-	PORTA = 0X00;
-	PORTC = 0X00;
-	PORTE = 0X00;
-	PORTD = 0b00000001;
-	PORTB = 0x01;
-	*/
+
 	while(1)
 	{
 		int i;
-		for (i=0;i<N_LEDS;i++)
+		for (i=0;i<N_LEDS;i++)		/*giro no sentido anti-horario acende um por vez*/
 		{
 			acender_led(i,vled);
-			Delay1KTCYx(5);
+			Delay1KTCYx(250);		// 250ms
 		}
-/*		Delay1KTCYx(5);	//Delay 1 x 1000 = 1.000 cyclos; XXXms @ 16MHz
+		
+		for (i=N_LEDS;i=0;i--)		/*giro no sentido horario acende um por vez*/
+		{
+			acender_led(i,vled);
+			Delay1KTCYx(250);		// 250ms
+		}		
+		
+/*		Delay1KTCYx(250);	//Delay  250 * 1000 * 1 = 250000 µs  = 0,25s = 250ms @ 4MHz
 		if(PORTB==0b10000000)
 		{
 			PORTB=0b00000001;
